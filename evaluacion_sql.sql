5db3f94d37281ea2670053b173e401dfbb473701
Maria G <mgg.guiso@gmail.com>
2025-03-12 16:07:40 +0100

    ej7-11

--- evaluacion_sql.sql
+++ evaluacion_sql.sql
@@ -90,3 +90,102 @@ FROM actor;
 SELECT first_name, actor_id
 FROM actor 
 WHERE actor_id BETWEEN 10 AND 20;
+
+
+-- 8. Encuentra el título de las películas en la tabla film que no sean ni "R" ni "PG-13" en cuanto a su clasificación.
+
+SELECT title
+FROM film
+WHERE rating NOT LIKE "R" AND rating NOT LIKE "PG-13";  
+
+-- TABLA UTILIZADA
+SELECT *
+FROM film;
+
+-- COMPROBACIÓN 
+SELECT title, rating
+FROM film
+WHERE rating NOT LIKE "R" AND rating NOT LIKE "PG-13";
+
+
+-- 9. Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento.
+
+SELECT rating, COUNT(*) AS cantidad_total_peliculas  -- COUNT(*) cuenta todas las peliculas que hay en rating. Utilizamos AS para renombrar la columna con los resultados y hacerlo más amigable
+FROM film
+GROUP BY rating;                                     -- Agrupamos por su clasificación. Siempre que realizamos una operación, en este caso count, es necesario utilizar group by.
+
+-- TABLA UTILIZADA
+SELECT *
+FROM film;
+
+
+-- 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.
+
+SELECT c.customer_id,c.first_name,c.last_name, COUNT(r.rental_id) AS total_peliculas_alquiladas  -- Contamos el total de peliculas alquiladas (función agregada)y le asignamos un alias a la columna
+FROM customer AS c
+INNER JOIN rental as r  -- Unimos la tabla customer y rental por la columna común customer_id.
+USING (customer_id)
+GROUP BY c.customer_id,c.first_name,c.last_name;
+
+
+-- TABLAS UTILIZADAS
+SELECT *
+FROM customer;
+
+SELECT *
+FROM rental;
+
+
+-- 11. Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el recuento de alquileres.
+
+-- SOLUCIÓN FINAL:
+
+SELECT 
+    c.category_id AS categoria,  
+    c.name AS nombre,                   
+    COUNT(rental_id) AS total_peliculas_alquiladas 
+    
+	FROM rental AS r              
+	INNER JOIN inventory AS i    -- Unimos las tablas rental e inventory por la columna común inventory_id
+	ON r.inventory_id = i.inventory_id
+    
+	INNER JOIN film AS f           -- Unimos las tablas inventory y film por la columna común film_id
+	ON i.film_id = f.film_id
+    
+	INNER JOIN film_category AS fc -- Unimos las tablas film y film_category (vinculamos estas tablas porque category no puede vincularse por sí misma a film ya que no comparten columna equiparable)
+	ON f.film_id = fc.film_id
+    
+	INNER JOIN category as c       -- Unimos las tablas film_category y category (ya que el nombre de la categoria se encuentra en la tabla category)
+	ON fc.category_id = c.category_id
+	GROUP BY categoria, nombre;
+
+
+
+-- FALTA QUÉ DATOS QUEREMOS SACAR(SELECT) Y SU AGRUPACIÓN (GROUP BY)
+
+SELECT *
+	FROM rental AS r
+	INNER JOIN inventory AS i
+	ON r.inventory_id = i.inventory_id
+    
+	INNER JOIN film AS f
+	ON f.film_id = i.film_id
+    
+	INNER JOIN film_category AS fc
+	ON f.film_id = fc.film_id
+    
+	INNER JOIN category as c
+	ON fc.category_id = c.category_id;
+    
+-- TABLAS UTILIZADAS
+SELECT *
+FROM rental;
+
+SELECT *
+FROM inventory;
+
+SELECT *
+FROM film;
+
+SELECT *
+FROM film_category;
